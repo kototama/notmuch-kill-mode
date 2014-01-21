a9d13cd9088a971b1f4dbe5590e13cedcac86d09
kototama <kototamo@gmail.com>
2014-01-21 10:31:06 +0100

    updated comments

--- notmuch-kill-mode.el
+++ notmuch-kill-mode.el
@@ -1,3 +1,39 @@
+;;; notmuch-kill-mode.el --- Minor mode to mute notmuch threads
+
+;; Copyright © 2013-2014 Kototama
+;;
+;; Author: Kototama <kototamo@gmail.com>
+;;
+;;
+;; URL: http://github.com/kototama/notmuch-kill-mode
+;; Version: 0.1
+;; Keywords: mail, notmuch
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
 (define-minor-mode notmuch-kill-mode
   "Toggle Notmuch Kill Mode.
      Allows the killing of message threads. All new incoming
@@ -116,3 +152,5 @@ read."
   (message "Kill-list processed."))
 
 (provide 'notmuch-kill-mode)
+
+;;; notmuch-kill-mode.el ends here
